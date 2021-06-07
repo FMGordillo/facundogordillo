56cad2ecdf5fb50c35a749de3de47734db490fd1
Facundo Martin Gordillo <facundomgordillo@gmail.com>
2021-06-06 22:00:50 -0300

    :tada: First commit

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Estas ahora en {{ route }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    route: 'AKSDOKASODKASOD',
+  }),
+})
+</script>
